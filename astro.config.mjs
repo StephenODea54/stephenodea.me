e8369033e90b7ab59cba354581be8b81e72568e1
StephenODea54 <odeastephen1@gmail.com>
2024-08-12 05:39:40 -0400

    add styling and remark themes for markdown files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,22 @@
 import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
-import mdx from "@astrojs/mdx";
+import rehypePrettyCode from "rehype-pretty-code";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   integrations: [tailwind({
     applyBaseStyles: false
-  }), react(), mdx()]
+  }), react(), mdx()],
+  markdown: {
+    syntaxHighlight: false,
+    rehypePlugins: [[rehypePrettyCode, {
+      theme: {
+        light: "min-light",
+        dark: "min-dark",
+      },
+    }]],
+  },
 });
---
+++ src/content/notes/dsa.md
@@ -0,0 +1,22 @@
+---
+layout: ../../layouts/Layout.astro
+description: Data Structures and Algorithms notes, written in Python
+lastModifiedDate: '2024-08-12'
+title: Data Structures and Algorithms
+url: notes/dsa
+---
+
+# Hi there!
+
+This Markdown file creates a page at `your-domain.com/page-1/`
+
+It probably isn't styled much, but Markdown does support:
+
+-   **bold** and _italics._
+-   lists
+-   [links](https://astro.build)
+-   and more!
+
+```typescript
+console.log(2);
+```
